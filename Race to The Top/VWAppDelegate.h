2cb94a7568c1d3dde062b9122d60677826910539
Valerino Wijaya <valerino@gmail.com>
2014-07-15 11:51:18 +0700

    Initial Commit

---
+++ Race to The Top/VWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VWAppDelegate.h
+//  Race to The Top
+//
+//  Created by Valerino on 7/15/14.
+//  Copyright (c) 2014 VW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Race to The Top/VWViewController.m
@@ -0,0 +1,29 @@
+//
+//  VWViewController.m
+//  Race to The Top
+//
+//  Created by Valerino on 7/15/14.
+//  Copyright (c) 2014 VW. All rights reserved.
+//
+
+#import "VWViewController.h"
+
+@interface VWViewController ()
+
+@end
+
+@implementation VWViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
